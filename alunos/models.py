eca829c1161f6d2bcca7b6d63ff0800f68cdc994
ian0011 <ianmoura607@gmail.com>
2021-07-09 18:04:22 -0300

    Add Alunos move archives

---
+++ alunos/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100, null=False)
+    cpf = models.CharField(max_length=14, null=False, verbose_name="CPF")
+    email = models.EmailField(max_length=254, null=False)
+    telefone = models.CharField(max_length=16, null=True)
+    created_date = models.DateField(default=timezone.now)
