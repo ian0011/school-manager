d122403b54588e8a9c0bf84aae86dbccf8c85f89
ian0011 <ianmoura607@gmail.com>
2021-07-13 12:08:41 -0300

    Static files in urls archive

--- classmanagement/urls.py
+++ classmanagement/urls.py
@@ -1,5 +1,4 @@
-"""classmanagement URL Configuration
-
+""""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -15,8 +14,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alunos.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
